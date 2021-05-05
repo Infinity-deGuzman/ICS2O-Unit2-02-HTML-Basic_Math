f908f181ff1c0c8cf085761c9813cd8924f1a8b2
InfinitydeGuzma <>
2021-05-05 18:01:54 +0000

    Added index.html, script.js, and style.css

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// Copyright (c) 2021 Infinity de Guzman All rights reserved
+//
+// Created by: Infinity de Guzman
+// Created on: May 2021
+// This file contains the JS functions for index.html
+
+function doMathClicked () {
+  // this function does basic math
+
+  document.getElementById('add-math').innerHTML = '6 + 5 = ' + (6 + 5)
+  document.getElementById('subtract-math').innerHTML = '7 - 3 =  ' + (7 - 3)
+  document.getElementById('multiply-math').innerHTML = '3 + 4 * 2 = ' + (3 + 4 * 2)
+  document.getElementById('divide-math').innerHTML = '4 / 2 + 3 = ' + ((4 / 2) + 3)
+  document.getElementById('exponent-math').innerHTML = '5 + 2 ** 3 = ' + (5 + 2 ** 3)
+}
