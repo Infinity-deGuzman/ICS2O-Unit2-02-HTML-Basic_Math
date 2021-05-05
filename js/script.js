28d290a08d520c082051743bd8ec3457f67e1c2f
InfinitydeGuzma <>
2021-05-05 20:14:28 +0000

    Made it calculate the perimeter and area (like it's supposed to)

--- js/script.js
+++ js/script.js
@@ -4,12 +4,8 @@
 // Created on: May 2021
 // This file contains the JS functions for index.html
 
-function doMathClicked () {
-  // this function does basic math
-
-  document.getElementById('add-math').innerHTML = '6 + 5 = ' + (6 + 5)
-  document.getElementById('subtract-math').innerHTML = '7 - 3 =  ' + (7 - 3)
-  document.getElementById('multiply-math').innerHTML = '3 + 4 * 2 = ' + (3 + 4 * 2)
-  document.getElementById('divide-math').innerHTML = '4 / 2 + 3 = ' + ((4 / 2) + 3)
-  document.getElementById('exponent-math').innerHTML = '5 + 2³ = ' + (5 + 2 ** 3)
+function doMathClicked() {
+  // this does the math
+  document.getElementById('area').innerHTML = 'The area is: ' + (5*3) + 'cm²'
+  document.getElementById('perimeter').innerHTML = 'The perimeter is: ' + (2*(5+3)) + 'cm'
 }
