b8be6cb962be7cbe71e998c9788611e844692550
InfinitydeGuzma <>
2021-05-05 18:13:36 +0000

    changed exponent in js file

--- js/script.js
+++ js/script.js
@@ -11,5 +11,5 @@ function doMathClicked () {
   document.getElementById('subtract-math').innerHTML = '7 - 3 =  ' + (7 - 3)
   document.getElementById('multiply-math').innerHTML = '3 + 4 * 2 = ' + (3 + 4 * 2)
   document.getElementById('divide-math').innerHTML = '4 / 2 + 3 = ' + ((4 / 2) + 3)
-  document.getElementById('exponent-math').innerHTML = '5 + 2 ** 3 = ' + (5 + 2 ** 3)
+  document.getElementById('exponent-math').innerHTML = '5 + 2³ = ' + (5 + 2 ** 3)
 }
